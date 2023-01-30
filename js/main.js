b71b5ad6ff294448cdd06c454a338de1209f0eeb
kwonchanwooo <rlaalfk121@gmail.com>
2023-01-30 20:25:35 +0900

    1차 피드백 수정

--- js/main.js
+++ js/main.js
@@ -49,6 +49,7 @@ var swiper = new Swiper(".mySwiper", {
     },
     pagination: {
         el: ".swiper-pagination",
+        type: "progressbar",
         clickable: true,
         renderBullet: function (index, className) {
             return '<span class="' + className + '">' + (index + 1) + "</span>";
