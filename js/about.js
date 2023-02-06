e5f640cf9290ebbcbd1ae87cfa34a6e33ef5f8d7
kwonchanwooo <rlaalfk121@gmail.com>
2023-02-06 17:17:57 +0900

    about 사진 수정

--- js/about.js
+++ js/about.js
@@ -1,8 +1,16 @@
 const articles = document.querySelectorAll('.lists article')
+console.log(articles)
 let i = 0
 
 for (let el of articles) {
     let pic = el.querySelector('.pic')
-    pic.style.backgroundImage = `url(../img/department/dep${i + 1}.jpg)`;
+
+
+    pic.style.backgroundImage = `url(./img/department/dep${i + 1}.jpg)`;
     i++
 }
+
+// for (let i = 0; i++; i < articles.length) {
+//     let pic = articles.querySelector('.pic')
+//     pic.style.backgroundImage = `url(../img/department/dep${i + 1}.jpg)`
+// }
