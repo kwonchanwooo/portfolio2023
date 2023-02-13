88120016daa3dcafb2fd343919b2ab722317d9c8
kwonchanwooo <rlaalfk121@gmail.com>
2023-02-13 23:08:31 +0900

    about 분리 & 반응형

--- js/about.js
+++ js/about.js
@@ -1,13 +1,12 @@
-const articles = document.querySelectorAll('.lists article')
-console.log(articles)
-let i = 0
+const articles = document.querySelectorAll('.lists article');
+console.log(articles);
+let i = 0;
 
 for (let el of articles) {
-    let pic = el.querySelector('.pic')
+	let pic = el.querySelector('.pic');
 
-
-    pic.style.backgroundImage = `url(./img/department/dep${i + 1}.jpg)`;
-    i++
+	pic.style.backgroundImage = `url(./img/department/dep${i + 1}.jpg)`;
+	i++;
 }
 
 const vidList = document.querySelector('.vidList');
@@ -18,27 +17,27 @@ const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&ke
 const btnClose = document.querySelector('.btnClose');
 
 fetch(url)
-    .then((data) => {
-        return data.json();
-    })
-    .then((json) => {
-        let items = json.items;
-        console.log(items)
-        let result = '';
+	.then((data) => {
+		return data.json();
+	})
+	.then((json) => {
+		let items = json.items;
+		console.log(items);
+		let result = '';
 
-        items.map((el) => {
-            let title = el.snippet.title;
-            if (title.length > 26) {
-                title = title.substr(0, 26) + '...';
-            }
-            let con = el.snippet.description;
-            if (con.length > 80) {
-                con = con.substr(0, 80) + '...';
-            }
-            let date = el.snippet.publishedAt;
-            date = date.split('T')[0];
+		items.map((el) => {
+			let title = el.snippet.title;
+			if (title.length > 26) {
+				title = title.substr(0, 26) + '...';
+			}
+			let con = el.snippet.description;
+			if (con.length > 80) {
+				con = con.substr(0, 80) + '...';
+			}
+			let date = el.snippet.publishedAt;
+			date = date.split('T')[0];
 
-            result += `
+			result += `
         <article>
           <a href="${el.snippet.resourceId.videoId}" class="pic">
             <img src="${el.snippet.thumbnails.standard.url}">
@@ -50,34 +49,32 @@ fetch(url)
           </div>
         </article>
       `;
-        });
+		});
 
-        vidList.innerHTML = result;
-    });
-
+		vidList.innerHTML = result;
+	});
 
 vidList.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (!e.target.closest('a')) return;
-    const vidId = e.target.closest('a').getAttribute('href');
+	e.preventDefault();
+	if (!e.target.closest('a')) return;
+	const vidId = e.target.closest('a').getAttribute('href');
 
-    let pop = document.createElement('figure');
-    pop.classList.add('pop');
-    pop.innerHTML = `
+	let pop = document.createElement('figure');
+	pop.classList.add('pop');
+	pop.innerHTML = `
         <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" width="100%" height="100%" allowfullscreen></iframe>
         <span class="btnClose"><i class="fa-regular fa-circle-xmark"></i></span>
         `;
 
-    vidList.append(pop);
+	vidList.append(pop);
 });
 
-
 vidList.addEventListener('click', (e) => {
-    const pop = vidList.querySelector('.pop');
-    if (pop) {
-        const close = pop.querySelector('span i');
-        if (e.target == close) pop.remove();
-    }
+	const pop = vidList.querySelector('.pop');
+	if (pop) {
+		const close = pop.querySelector('span i');
+		if (e.target == close) pop.remove();
+	}
 });
 
 //loc
@@ -87,54 +84,47 @@ const buttons = document.querySelectorAll('.chain li');
 const t_on = document.querySelectorAll('.traffic li')[0];
 const t_off = document.querySelectorAll('.traffic li')[1];
 
-
-
 var options = {
-
-    center: new kakao.maps.LatLng(37.5347906, 126.9935937),
-    level: 3,
+	center: new kakao.maps.LatLng(37.5347906, 126.9935937),
+	level: 3,
 };
 
 var map = new kakao.maps.Map(container, options);
 
 let markerOptions = [
-
-    {
-        title: '서울',
-        latlng: new kakao.maps.LatLng(37.5347906, 126.9935937),
-        button: buttons[0],
-    },
-    {
-        title: '대구',
-        latlng: new kakao.maps.LatLng(35.8252327, 128.6203849),
-        button: buttons[1],
-    }
+	{
+		title: '서울',
+		latlng: new kakao.maps.LatLng(37.5347906, 126.9935937),
+		button: buttons[0],
+	},
+	{
+		title: '대구',
+		latlng: new kakao.maps.LatLng(35.8252327, 128.6203849),
+		button: buttons[1],
+	},
 ];
 
-
-
 for (let i = 0; i < markerOptions.length; i++) {
-    new kakao.maps.Marker({
-        map: map,
-        position: markerOptions[i].latlng,
-        title: markerOptions[i].title,
+	new kakao.maps.Marker({
+		map: map,
+		position: markerOptions[i].latlng,
+		title: markerOptions[i].title,
+	});
 
-    });
+	markerOptions[i].button.addEventListener('click', (e) => {
+		e.preventDefault();
+		for (let k = 0; k < markerOptions.length; k++) {
+			markerOptions[k].button.classList.remove('on');
+		}
+		markerOptions[i].button.classList.add('on');
 
-    markerOptions[i].button.addEventListener('click', (e) => {
-        e.preventDefault();
-        for (let k = 0; k < markerOptions.length; k++) {
-            markerOptions[k].button.classList.remove('on');
-        }
-        markerOptions[i].button.classList.add('on');
-
-        moveTo(markerOptions[i].latlng);
-    });
+		moveTo(markerOptions[i].latlng);
+	});
 }
 
 function moveTo(target) {
-    var moveLatLon = target;
-    map.setCenter(moveLatLon);
+	var moveLatLon = target;
+	map.setCenter(moveLatLon);
 }
 
 var mapTypeControl = new kakao.maps.MapTypeControl();
